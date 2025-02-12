096950c21eb144a4ff4dfbde4bbbf6bc390a0e06
Ian Page Maciel <ianpage27@gmail.com>
2025-02-12 20:04:47 -0300

    contador

---
+++ contador.js
@@ -0,0 +1,23 @@
+const mais = document.getElementById('mais')
+const menos = document.getElementById('menos')
+const zerar = document.getElementById('limpar')
+const res = document.getElementById('contagem') 
+
+let contagemAtual = 0 
+
+mais.addEventListener('click',function(){
+    contagemAtual++
+    res.innerHTML = contagemAtual
+}) 
+
+
+menos.addEventListener('click',function(){
+    contagemAtual--
+    res.innerHTML = contagemAtual
+})
+
+zerar.addEventListener('click',function(){
+    contagemAtual = 0
+    res.innerHTML = contagemAtual
+})
+document.querySelectorAll
